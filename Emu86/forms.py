41515d37d24f5b631ff32b8575c6d07f35c018ad
stevenkang312 <84217293+stevenkang312@users.noreply.github.com>
2025-11-25 00:01:04 -0500

    Updated register cell to Matt's style

--- Emu86/forms.py
+++ Emu86/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class MainForm(forms.Form):
     code = forms.CharField(required=False,
-                           widget=forms.Textarea(attrs={'cols': 60,
+                           widget=forms.Textarea(attrs={'cols': 40,
                                                         'rows': 24,
                                                         'oninput':
                                                         'checkForScript()'}))
