41f9f54b8dd3633ab7df82a56cd3fa16d691bf26
gcallah <gcallah@mac.com>
2025-12-01 13:54:33 -0500

    More tweaking to main page.

--- Emu86/forms.py
+++ Emu86/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 
+CODE_COLS = 48
+CODE_ROWS = 48
 
 class MainForm(forms.Form):
     code = forms.CharField(required=False,
-                           widget=forms.Textarea(attrs={'cols': 40,
-                                                        'rows': 24,
+                           widget=forms.Textarea(attrs={'cols': CODE_COLS,
+                                                        'rows': CODE_ROWS,
                                                         'oninput':
                                                         'checkForScript()'}))
