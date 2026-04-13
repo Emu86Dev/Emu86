844b1c1ff2212f932000d1c8df4dcfb259d196c9
stevenkang312 <84217293+stevenkang312@users.noreply.github.com>
2026-04-12 22:50:07 -0400

    fixed lint style in main program

--- Emu86/templatetags/custom_tags.py
+++ Emu86/templatetags/custom_tags.py
@@ -3,10 +3,10 @@ from django.utils.html import format_html
 
 register = template.Library()
 
+
 @register.simple_tag
 def option_maybe_selected(val, text, selected):
     if val == selected:
         return format_html('<option value="{}" selected>{}</option>', val, text)
     else:
         return format_html('<option value="{}">{}</option>', val, text)
-
